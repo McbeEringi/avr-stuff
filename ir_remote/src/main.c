204ef63c29bf3eb3a919f3ce0aa8d46c268e3b29
McbeEringi <git@mcbeeringi.dev>
2025-01-19 12:07:18 +0900

    -

--- ir_remote/src/main.c
+++ ir_remote/src/main.c
@@ -1,9 +1,5 @@
-/*
-IR: tX02 ? PA2 : PB2
-LED: PA3
-SW_A: PA7
-SW_B: PA1
-*/
+// SW: PA6 PA7 PA1
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
@@ -22,11 +18,58 @@ const uint8_t code_d[]={0x2c,0x52,0x09,0x2f,0x26};// 消灯
 
 
 #define IR_HZ 38000
+#define IR_WO 0
+#define LED_PORT PORTA
+#define LED_PIN 2
+
+
+#ifdef REV1
+	#define TINY_XY2
+	#define IR_WO 2
+	#define LED_PORT PORTA
+	#define LED_PIN 3
+#endif
+#ifdef REV2
+	#define TINY_XY2
+	#define IR_WO 0
+	#define LED_PORT PORTA
+	#define LED_PIN 2
+#endif
+
+////////////////////////////////////////////////////////////
+
+#if defined(TINY_XY2)||defined(MEGAAVR_SERIES)
+	#define IR_PORT PORTA
+#else
+	#define IR_PORT PORTB
+#endif
+
+#if IR_WO==0
+	#ifdef TINY_14_20_PIN
+		#define IR_PIN 0
+	#else
+		#define IR_PIN 3
+	#endif
+	#define IR_CMP_BM TCA_SINGLE_CMP0EN_bm
+	#define IR_CMPnBUF CMP0BUF
+#elif IR_WO==1
+	#define IR_PIN IR_WO
+	#define IR_CMP_BM TCA_SINGLE_CMP1EN_bm
+	#define IR_CMPnBUF CMP1BUF
+#elif IR_WO==2
+	#define IR_PIN IR_WO
+	#define IR_CMP_BM TCA_SINGLE_CMP2EN_bm
+	#define IR_CMPnBUF CMP2BUF
+#else
+	#error "IR_WO out of 0 ~ 2"
+#endif
+
+
 #define IR_TOP (F_CPU+IR_HZ/2)/IR_HZ
-#define IR_ON TCA0.SINGLE.CMP0BUF=(IR_TOP+3/2)/3-1// duty=1/3
-#define IR_OFF TCA0.SINGLE.CMP0BUF=0
-#define LED_ON PORTA.OUTSET=1<<2
-#define LED_OFF PORTA.OUTCLR=1<<2
+#define IR_ON TCA0.SINGLE.IR_CMPnBUF=(IR_TOP+3/2)/3-1// duty=1/3
+#define IR_OFF TCA0.SINGLE.IR_CMPnBUF=0
+#define LED_ON LED_PORT.OUTSET=1<<LED_PIN
+#define LED_OFF LED_PORT.OUTCLR=1<<LED_PIN
 #define FOR(X) for(uint8_t i=0;i<X;++i)
 void set_wait(t){TCB0.CNT=0;TCB0.CCMP=(F_CPU*t+500000)/1000000-1;}// TCB0 TOP
 void wait(){while(!(TCB0.INTFLAGS&TCB_CAPT_bm));TCB0.INTFLAGS=1;}// TCB0
@@ -48,19 +91,16 @@ void main(){
 
 	// TCA0 IR 1333kHz/35=38.095kHz
 	TCA0.SINGLE.CTRLA=TCA_SINGLE_ENABLE_bm;
-	TCA0.SINGLE.CTRLB=TCA_SINGLE_CMP0EN_bm|TCA_SINGLE_WGMODE_SINGLESLOPE_gc;
+	TCA0.SINGLE.CTRLB=IR_CMP_BM|TCA_SINGLE_WGMODE_SINGLESLOPE_gc;
 	TCA0.SINGLE.PER=IR_TOP-1;// TOP 35-1
 
 	// TCB0 delay
 	TCB0.CTRLA=TCB_ENABLE_bm;
 	TCB0.CCMP=1000;// init any
 
-	#ifdef USE_PB2
-		PORTA.DIRSET=0b1000;// out: PA3
-		PORTB.DIRSET=0b0100;// out: PB2
-	#else
-		PORTA.DIRSET=0b1100;// out: PA2,3
-	#endif
+	IR_PORT.DIRSET=1<<IR_PIN;
+	LED_PORT.DIRSET=1<<LED_PIN;
+
 	PORTA.PIN6CTRL=PORT_PULLUPEN_bm|PORT_ISC_BOTHEDGES_gc;// BOTHEDGES|LEVEL
 	PORTA.PIN7CTRL=PORT_PULLUPEN_bm|PORT_ISC_BOTHEDGES_gc;// BOTHEDGES|LEVEL
 	PORTA.PIN1CTRL=PORT_PULLUPEN_bm|PORT_ISC_BOTHEDGES_gc;// BOTHEDGES|LEVEL
