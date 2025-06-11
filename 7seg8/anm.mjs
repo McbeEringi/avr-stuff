2c4a96164417258f611d092c577592b47d62d5a6
McbeEringi <git@mcbeeringi.dev>
2025-06-12 02:15:48 +0900

    -

--- 7seg8/anm.mjs
+++ 7seg8/anm.mjs
@@ -5,7 +5,7 @@ const
 s=[
 	...`        HELLO! THIS IS A TEST PARAGRAPH FOR 7SEG DISPLAY. IT USES ONLY CHARS THAT CAN BE DISPLAYED. A B C D E F G H I J L N O P Q R S T U Y 0 1 2 3 4 5 6 7 8 9 . - _ ^ = ( ) / \\ ? ! . INPUT = UART or I2C. PROCESSOR = ATtiny202. 74HC595 RC FILTER ONE-CABLE CONNECTION. 1000us/DIGIT. 125FPS. `,
 	...`        S    P    A    C    E `,
-].map((x,i,a)=>({seg:c2seg(x),bri:a[i-1]==' '?1:0})),
+].map((x,i,a)=>({seg:c2seg(x),bri:a[i-1]==' '?2:1})),
 w=Bun.file('/dev/ttyUSB0').writer();
 
 let i=0;
