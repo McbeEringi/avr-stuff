97caa06502fe7bb582a07c2eb46c56821d0e09a1
McbeEringi <git@mcbeeringi.dev>
2025-06-11 21:06:08 +0900

    -

--- 7seg8/anm.mjs
+++ 7seg8/anm.mjs
@@ -1,12 +1,15 @@
 #!/usr/bin/bun
 import{s2seg}from'./s2seg.mjs';
 
-const w=[
+const s=[
 	...`        HELLO! THIS IS A TEST PARAGRAPH FOR 7SEG DISPLAY. IT USES ONLY CHARS THAT CAN BE DISPLAYED. A B C D E F G H I J L N O P Q R S T U Y 0 1 2 3 4 5 6 7 8 9 . - _ ^ = ( ) / \\ ? ! . INPUT = UART or I2C. PROCESSOR = ATtiny402. 74HC595 RC FILTER ONE-CABLE CONNECTION. 500us/DIGIT. 250FPS. `,
 	...`        S    P    A    C    E `,
-].map(s2seg);
+].map(s2seg),
+w=Bun.file('/dev/ttyUSB0').writer();
+
 let i=0;
 setInterval(_=>(
-	Bun.write('/dev/ttyUSB0',new Uint8Array(w.slice(i,64+i++))),
-	w.length<i&&(i=0)
+	w.write(new Uint8Array(s.slice(i,64+i++))),
+	w.flush(),
+	s.length<i&&(i=0)
 ),200);
--- 7seg8/conv.mjs
+++ 7seg8/conv.mjs
@@ -1,6 +1,10 @@
 #!/usr/bin/bun
 import{s2seg}from'./s2seg.mjs';
 
-await Bun.stdout.write(
-	new Uint8Array([...(await Bun.stdin.text())].slice(0,-1).map(s2seg))
-);
+// await Bun.stdout.write(
+const 
+	x=new Uint8Array([...(await Bun.stdin.text())].slice(0,-1).map(s2seg)),
+	w=Bun.file('/dev/ttyUSB0').writer();
+
+w.write(x);
+w.flush();
