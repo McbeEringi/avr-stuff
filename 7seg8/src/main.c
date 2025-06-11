f062e8b974ad4c86170a20031549b530f3fe34cd
McbeEringi <git@mcbeeringi.dev>
2025-06-11 17:08:00 +0900

    -

--- 7seg8/src/main.c
+++ 7seg8/src/main.c
@@ -3,14 +3,26 @@
 #include <avr/interrupt.h>
 #define BAUD_RATE(X) (4.*F_CPU/(X)+.5)
 #define U_SEC(X) (F_CPU/10000/(X)-1)
+#define FOR(X) for(uint8_t i=0;i<X;i++)
 
-
+#if defined(REV1)
+#define LSBFIRST
 #define SERCLK 1
 #define RCLK 2
 #define OE_ 3
 #define TXD 6
 #define RXD 7
-#define FOR(X) for(uint8_t i=0;i<X;i++)
+#elif defined(REV2)
+#define MSBFIRST
+#define UART_ALT
+#define SERCLK 6
+#define RCLK 7
+#define OE_ 3
+#define TXD 1
+#define RXD 2
+#else
+#error "REV?"
+#endif
 
 volatile uint8_t disp[8]={// -HELLO!-
 	2,
@@ -47,11 +59,14 @@ void main(){
 	TCA0.SINGLE.CTRLA=TCA_SINGLE_ENABLE_bm;
 	TCA0.SINGLE.CTRLB=TCA_SINGLE_CMP0EN_bm|TCA_SINGLE_WGMODE_SINGLESLOPE_gc;
 	TCA0.SINGLE.PER=255;
-	TCA0.SINGLE.CMP0BUF=0;
+	TCA0.SINGLE.CMP0BUF=224;
 
 	TCB0.CTRLA=TCB_ENABLE_bm;
 
 	// 11500 8N1
+#ifdef UART_ALT
+	PORTMUX.CTRLB=PORTMUX_USART0_ALTERNATE_gc;
+#endif
 	USART0.BAUD=BAUD_RATE(115200);// 9600 too slow !
 	USART0.CTRLB=USART_RXEN_bm|USART_TXEN_bm;
 	USART0.CTRLA=USART_RXCIE_bm;
@@ -64,10 +79,18 @@ void main(){
 		TCB0.CCMP=U_SEC(10);
 		// >> ABCDEFGd 01234567
 		uint16_t w=(disp[i]<<8)|((1<<(7-i))^255);
-		for(uint8_t j=0;j<16;++j){// 10*3*16=480 us
+		for(uint8_t
+#if defined(LSBFIRST)
+			j=0;j<16;++j
+#elif defined(MSBFIRST)
+			j=15;j<16;--j
+#else
+#error "bit order?"
+#endif
+			){// 10*3*16=480 us
 			wait();
 			PORTA.OUTCLR=_BV(SERCLK);
-			if(((w>>j)&1)){PORTA.OUTSET=_BV(SERCLK);wait();}
+			if((w>>j)&1){PORTA.OUTSET=_BV(SERCLK);wait();}
 			else{wait();PORTA.OUTSET=_BV(SERCLK);}
 			wait();
 		}
