53dee3c2bb2f8cfa638d2db414fbb582da9c07e6
McbeEringi <git@mcbeeringi.dev>
2026-04-14 07:03:48 +0900

    -

--- uart2i2c/anm.mjs
+++ uart2i2c/anm.mjs
@@ -43,20 +43,24 @@ knzw=x=>[
 		0b00101010,0b01001001,
 		0b01111111,0b01010001,
 ],
-l=10;
+l=12;
 
 
 let i=0;
 setInterval(s=>(
-	s=[
-		...` HELLO! THIS IS A TEST PARAGRAPH FOR 7SEG DISPLAY. IT USES ONLY CHARS THAT CAN BE DISPLAYED. A B C D E F G H I J L N O P Q R S T U Y 0 1 2 3 4 5 6 7 8 9 . - _ ^ = ( ) / \\ ? ! . INPUT = UART or I2C. PROCESSOR = ATtiny202. 74HC595 RC FILTER ONE-CABLE CONNECTION. 1000us/DIGIT. 125FPS. `,
+	// s=[
+		// ...` HELLO! THIS IS A TEST PARAGRAPH FOR 7SEG DISPLAY. IT USES ONLY CHARS THAT CAN BE DISPLAYED. A B C D E F G H I J L N O P Q R S T U Y 0 1 2 3 4 5 6 7 8 9 . - _ ^ = ( ) / \\ ? ! . INPUT = UART or I2C. PROCESSOR = ATtiny202. 74HC595 RC FILTER ONE-CABLE CONNECTION. 1000us/DIGIT. 125FPS. `,
 		//...`        S    P    A    C    E `,
 		// ...'    ',
 		// ...`5000000000000000 YEN HOSII!!!`,
 		// ...'    ',
 		// ...new Date().toISOString().replace(/:/g,"'").slice(0,-1),
-		...'    ',
-	].map((x,i,a)=>({seg:c2seg(x),bri:!i||a[i-1]==' '?2:1})),
+		// ...'    ',
+	// ].map((x,i,a)=>({seg:c2seg(x),bri:!i||a[i-1]==' '?2:1})),
+	s=[
+		...' 8888888',
+		// ...'                '
+	].map((x,i)=>({seg:c2seg(x),bri:[0,0,1,2,3,2,1,0][i]||0})),
 	w.write(new Uint8Array([
 		...a2b([...Array(Math.ceil(8*l/s.length)+1)].flatMap(_=>s).slice(++i),0x10),
 		...(i&0b11?[]:[...nt((i>>>2)&1?0x21:0x20),...knzw((i>>>2)&1?0x20:0x21)])
